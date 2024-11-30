ce5b336d62da5da518b3cba7dc2484e1a5058e1f
TruongVanMinh <vanminhtruong95@gmail.com>
2024-11-30 14:26:47 +0700

    project

---
+++ game/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Hello Minh</h1>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
